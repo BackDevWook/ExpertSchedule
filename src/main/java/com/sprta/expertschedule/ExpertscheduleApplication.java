cffb6a1ff8b9c26e083b3b93ff4f0f744af64fca
BackDevWook <https://github.com/BackDevWook>
2025-03-28 16:54:20 +0900

    스프링 숙련 프로젝트 생성

---
+++ src/main/java/com/sprta/expertschedule/ExpertscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sprta.expertschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertscheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertscheduleApplication.class, args);
+    }
+
+}
